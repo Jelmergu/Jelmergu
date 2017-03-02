fbf8c8d38f1b140402b686828f8e55d3bb9700b1
Jelmergu <jelmergu@gmail.com>
2017-03-02 10:03:14 +0100

    Added add and subtract to Date
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- src/Date.php
+++ src/Date.php
@@ -9,7 +9,7 @@
 
 namespace Jelmergu;
 
-use \DateTime;
+use DateTime;
 
 /**
  * The class Date contains additional options for date
@@ -20,10 +20,11 @@ class Date extends DateTime
 {
     /**
      * Changes the date(m) value to the dutch version of date(F)
+     * v0.1
      *
      * @param  string $month Numeric representation of a month, with leading zeros as returned by date(m)
      *
-     * @return string           A full textual representation of a month in the dutch language similar to date(F)
+     * @return string  A full textual representation of a month in the dutch language similar to date(F)
      */
     public function monthToString(string $month) : string
     {
@@ -37,5 +38,125 @@ class Date extends DateTime
         ];
 
         return $months[$month];
+    }
+
+    /**
+     * Add or subtract specified years from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $years The years to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addYears(int $years) : Date
+    {
+        $meth = "add";
+        if ($years < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("P" . sqrt(pow($years, 2)) . "Y"));
+
+        return $this;
+    }
+
+    /**
+     * Add or subtract specified months from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $months The months to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addMonths(int $months) : Date
+    {
+        $meth = "add";
+        if ($months < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("P" . sqrt(pow($months, 2)) . "M"));
+
+        return $this;
+    }
+
+    /**
+     * Add or subtract specified days from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $days The days to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addDays(int $days) : Date
+    {
+        $meth = "add";
+        if ($days < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("P" . sqrt(pow($days, 2)) . "D"));
+
+        return $this;
+    }
+
+    /**
+     * Add or subtract specified hours from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $hours The hours to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addHours(int $hours) : Date
+    {
+        $meth = "add";
+        if ($hours < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("PT" . sqrt(pow($hours, 2)) . "H"));
+
+        return $this;
+    }
+
+    /**
+     * Add or subtract specified minutes from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $minutes The minutes to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addMinutes(int $minutes) : Date
+    {
+        $meth = "add";
+        if ($minutes < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("PT" . sqrt(pow($minutes, 2)) . "M"));
+
+        return $this;
+    }
+
+    /**
+     * Add or subtract specified seconds from the current date
+     *
+     * @version 1.0.5
+     *
+     * @param int $seconds The seconds to add or subtract. Subtraction is specified with a minus
+     *
+     * @return Date
+     */
+    public function addSeconds(int $seconds) : Date
+    {
+        $meth = "add";
+        if ($seconds < 0) {
+            $meth = "sub";
+        }
+        $this->$meth(new \DateInterval("PT" . sqrt(pow($seconds, 2)) . "S"));
+
+        return $this;
     }
 }
