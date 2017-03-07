5252832200ad33d8a3b8cb999cab2163aa014b71
Jelmergu <jelmergu@gmail.com>
2017-03-07 10:12:03 +0100

    Added between function Added between function test(unknown if correct)
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- src/Date.php
+++ src/Date.php
@@ -38,4 +38,17 @@ class Date extends DateTime
 
         return $months[$month];
     }
+    
+    /**
+     * Check if the current date is between the input dates
+     *
+     * @param string $lowerDate The beginning of the date range
+     * @param string $upperDate The end of the date range
+     *
+     * @return bool
+     */
+    public function between(string $lowerDate, string $upperDate) : bool
+    {
+        return ($this->getTimestamp() >= strtotime($lowerDate) && $this->getTimestamp() <= strtotime($upperDate));
+    }
 }
