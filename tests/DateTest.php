67d56aec9ec536d76aac008b84c110d12c036336
Jelmergu <jelmergu@gmail.com>
2017-03-08 08:20:41 +0100

    Corrected DateTest
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -14,9 +14,9 @@ class DateTest extends TestCase
 
     public function test_date_in_range()
     {
-        $inrange = (new Date(strtotime("2017-02-09")))->between('2017-02-08', "2017-02-10");
-        $beforerange = (new Date(strtotime("2017-02-09")))->between('2017-02-06', "2017-02-08");
-        $afterrange = (new Date(strtotime("2017-02-09")))->between('2017-02-12', "2017-02-10");
+        $inrange = (new Date("2017-02-09"))->between('2017-02-08', "2017-02-10");
+        $beforerange = (new Date("2017-02-09"))->between('2017-02-06', "2017-02-08");
+        $afterrange = (new Date("2017-02-09"))->between('2017-02-12', "2017-02-10");
 
         $this->assertTrue($inrange);
         $this->assertFalse($beforerange);
