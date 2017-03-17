d28df11b00c26a0ad19d01ea3c6fd0fa9e359d5e
Jelmergu <jelmergu@gmail.com>
2017-03-17 11:55:20 +0100

    Added crypt to password hash function
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- src/Hashing.php
+++ src/Hashing.php
@@ -13,32 +13,53 @@ class Hashing
 {
 
     /**
-     * Check if the input hash is not a passwordHash and if it matches the md5 hash of input
-     * Convert the input to password_hashed hash if both are true
+     * Validates the input password if it is md5 and converts hash to password_hash
      *
      * @version v1.0.1
      *
-     * @param string $hash  The hash to check. Is passed as reference so will contain the resulting hash
-     * @param string $input The plain text to check the hash against
+     * @param string $password The plain text to check the hash against
+     * @param string $hash     The hash to check. Is passed as reference so will contain the resulting hash
      *
-     * @return bool
+     * @return bool false if no conversion was made, true otherwise
      */
-    public static function md5ToPassHash(string &$hash, string $input): bool
+    public static function md5ToPassHash(string $password, string &$hash) : bool
     {
 
-        if (self::isPasswordHash($hash) === false) {
-            if ($hash == md5($input)) {
-                $hash = password_hash($input, PASSWORD_DEFAULT);
+        if ($hash == md5($password)) {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
 
-                return true;
-            }
+            return TRUE;
         }
 
-        return false;
+        return FALSE;
+    }
+
+    /**
+     * Validates the input password and converts to password_hash
+     *
+     * @version v1.0.6
+     *
+     * @param string $password The plain text password to check against
+     * @param string $hash     The hash to compare
+     * @param string $salt     The optional salt passed to crypt. Will not be used with password_hash
+     *
+     * @return bool false if no conversion was made, true otherwise
+     */
+    public static function cryptToPassHash(string $password, string &$hash, string $salt = "")
+    {
+        if (crypt($password, $salt) == $hash) {
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            return TRUE;
+        }
+
+        return FALSE;
     }
 
     /**
      * Check if the input hash was created using password_hash
+     *
+     * @note    Unknown if always correct
      *
      * @version v1.0.1
      *
@@ -46,7 +67,7 @@ class Hashing
      *
      * @return bool
      */
-    public static function isPasswordHash(string $hash): bool
+    public static function isPasswordHash(string $hash) : bool
     {
         return preg_match("`\\$2y\\$10\\$`", $hash) > 0;
     }
