1930538a96e7ef2199011a61261d0935991c315a
Jelmergu <jelmergu@gmail.com>
2017-03-30 15:04:18 +0200

    Added correction to Log::setLogLocation Added debugLog
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- src/Log.php
+++ src/Log.php
@@ -16,25 +16,53 @@ class Log
 
     public static function setLogLocation(string $path)
     {
-        $aPWD = preg_split("`\\|/`", $path);
+        $aPWD = preg_split("`/|\\\`", $path);
         $newPath = "";
         foreach ($aPWD as $sPWD) {
-            $newPath .= $sPWD . DIRECTORY_SEPARATOR;
+            if (strlen($sPWD) > 0) {
+                $newPath .= $sPWD . DIRECTORY_SEPARATOR;
+            }
         }
-        if (is_writable($newPath) === false) {
+        if (is_writable($newPath) === FALSE) {
             throw new InvalidFileSystemRights(222, $newPath);
         }
         self::$logLocation = $newPath;
     }
 
-    public static function DatabaseLog($message)
+    protected static function writeLog($logName, $message, $extra)
     {
-        $file = fopen(self::$logLocation."Database.log", "a");
-        fwrite($file, $message);
+        $file = fopen(self::$logLocation . $logName.".log", "a");
+        fwrite($file, self::prepareMessage($message));
+        if (isset($extra[0][0]) === true){
+            foreach ($extra[0] as $message) {
+                fwrite($file, self::prepareMessage($message));
+            }
+        }
         fclose($file);
     }
 
-    private static function prepareMessage($message)
+
+    public static function databaseLog($message, ...$extra)
+    {
+        if (isset($extra[0]) === true){
+            self::writeLog("database", $message, $extra);
+        }
+        else {
+            self::writeLog("database", $message);
+        }
+    }
+
+    public static function debugLog($message, ...$extra)
+    {
+        if (isset($extra[0]) === true){
+            self::writeLog("debug", $message, $extra);
+        }
+        else {
+            self::writeLog("debug", $message);
+        }
+    }
+
+    private static function prepareMessage($file, string $message)
     {
         if (is_object($message) === TRUE || is_array($message)) {
             $message = json_encode($message);
@@ -43,7 +71,7 @@ class Log
 
         $message = "[" . $now->format("Y-m-d H:i:s:u") . "] " . $message . PHP_EOL;
 
-        return $message;
+        fwrite($file, $message);
     }
 
 }
