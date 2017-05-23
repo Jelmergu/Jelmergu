4e4349e7aeb6a89f38f375337b7fa13c342fe1c7
Jelmergu <Jelmergu@users.noreply.github.com>
2017-05-23 15:08:04 +0200

    Added Travis settings start
    Signed-off-by: Jelmergu <jelmergu@gmail.com>

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -25,22 +25,22 @@ class DateTest extends TestCase
 
     public function test_add_year() {
         
-        $oneMoreYear = (new Date("2017-01-01"))->addYears(1);
-        $oneLessYear = (new Date("2017-01-01"))->addYears(-1);
-        $tenMoreYears= (new Date("2017-01-01"))->addYears(10);
-        $tenLessYears= (new Date("2017-01-01"))->addYears(-10);
+        $oneMoreYear = (new Date("2017-01-01"))->addYears(1)->format("Y-m-d H:i:s");
+        $oneLessYear = (new Date("2017-01-01"))->addYears(-1)->format("Y-m-d H:i:s");
+        $tenMoreYears= (new Date("2017-01-01"))->addYears(10)->format("Y-m-d H:i:s");
+        $tenLessYears= (new Date("2017-01-01"))->addYears(-10)->format("Y-m-d H:i:s");
          
-        $this->assertEquals("2018-01-01", $oneMoreYear);
-        $this->assertEquals("2016-01-01", $oneLessYear);
-        $this->assertEquals("2027-01-01", $tenMoreYears);
-        $this->assertEquals("2007-01-01", $tenLessYears);
+        $this->assertEquals("2018-01-01 00:00:00", $oneMoreYear);
+        $this->assertEquals("2016-01-01 00:00:00", $oneLessYear);
+        $this->assertEquals("2027-01-01 00:00:00", $tenMoreYears);
+        $this->assertEquals("2007-01-01 00:00:00", $tenLessYears);
     }
 
     public function test_add_months () {
-        $oneMoreMonth = (new Date("2017-01-01"))->addMonths(1);
-        $oneLessMonth = (new Date("2017-01-01"))->addMonths(-1);
+        $oneMoreMonth = (new Date("2017-01-01"))->addMonths(1)->format("Y-m-d H:i:s");
+        $oneLessMonth = (new Date("2017-01-01"))->addMonths(-1)->format("Y-m-d H:i:s");
 
-        $this->assertEquals("2017-02-01", $oneMoreMonth);
-        $this->assertEquals("2016-12-01", $oneLessMonth);
+        $this->assertEquals("2017-02-01 00:00:00", $oneMoreMonth);
+        $this->assertEquals("2016-12-01 00:00:00", $oneLessMonth);
     }
 }
